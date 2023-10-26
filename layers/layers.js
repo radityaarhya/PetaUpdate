212a1c055992dbfcb546ee2e3b523b96b2e7ce1f
radityaarhya <radityaarhya48@gmail.com>
2023-10-26 22:20:53 +0800

    update layers

--- layers/layers.js
+++ layers/layers.js
@@ -42,120 +42,8 @@ var lyr_JURITEMERGENSItracks_2 = new ol.layer.Vector({
                 interactive: true,
                 title: '<img src="styles/legend/JURITEMERGENSItracks_2.png" /> JURIT-EMERGENSI — tracks'
             });
-var format_JalanDroppointAkeWelcoming_3 = new ol.format.GeoJSON();
-var features_JalanDroppointAkeWelcoming_3 = format_JalanDroppointAkeWelcoming_3.readFeatures(json_JalanDroppointAkeWelcoming_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_JalanDroppointAkeWelcoming_3 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_JalanDroppointAkeWelcoming_3.addFeatures(features_JalanDroppointAkeWelcoming_3);
-var lyr_JalanDroppointAkeWelcoming_3 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_JalanDroppointAkeWelcoming_3, 
-                style: style_JalanDroppointAkeWelcoming_3,
-                interactive: true,
-                title: '<img src="styles/legend/JalanDroppointAkeWelcoming_3.png" /> Jalan Droppoint-A ke Welcoming'
-            });
-var format_JURITPLANB_4 = new ol.format.GeoJSON();
-var features_JURITPLANB_4 = format_JURITPLANB_4.readFeatures(json_JURITPLANB_4, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_JURITPLANB_4 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_JURITPLANB_4.addFeatures(features_JURITPLANB_4);
-var lyr_JURITPLANB_4 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_JURITPLANB_4, 
-                style: style_JURITPLANB_4,
-                interactive: true,
-                title: '<img src="styles/legend/JURITPLANB_4.png" /> JURIT PLAN-B'
-            });
-var format_JURITPLANA_5 = new ol.format.GeoJSON();
-var features_JURITPLANA_5 = format_JURITPLANA_5.readFeatures(json_JURITPLANA_5, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_JURITPLANA_5 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_JURITPLANA_5.addFeatures(features_JURITPLANA_5);
-var lyr_JURITPLANA_5 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_JURITPLANA_5, 
-                style: style_JURITPLANA_5,
-                interactive: true,
-                title: '<img src="styles/legend/JURITPLANA_5.png" /> JURIT PLAN-A'
-            });
-var format_ShortcutPanitia_6 = new ol.format.GeoJSON();
-var features_ShortcutPanitia_6 = format_ShortcutPanitia_6.readFeatures(json_ShortcutPanitia_6, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_ShortcutPanitia_6 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_ShortcutPanitia_6.addFeatures(features_ShortcutPanitia_6);
-var lyr_ShortcutPanitia_6 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_ShortcutPanitia_6, 
-                style: style_ShortcutPanitia_6,
-                interactive: true,
-                title: '<img src="styles/legend/ShortcutPanitia_6.png" /> Shortcut Panitia'
-            });
-var format_NAVDAR12_7 = new ol.format.GeoJSON();
-var features_NAVDAR12_7 = format_NAVDAR12_7.readFeatures(json_NAVDAR12_7, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_NAVDAR12_7 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_NAVDAR12_7.addFeatures(features_NAVDAR12_7);
-var lyr_NAVDAR12_7 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_NAVDAR12_7, 
-                style: style_NAVDAR12_7,
-                interactive: true,
-                title: '<img src="styles/legend/NAVDAR12_7.png" /> NAVDAR 1-2'
-            });
-var format_JURITEMERGENSI_8 = new ol.format.GeoJSON();
-var features_JURITEMERGENSI_8 = format_JURITEMERGENSI_8.readFeatures(json_JURITEMERGENSI_8, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_JURITEMERGENSI_8 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_JURITEMERGENSI_8.addFeatures(features_JURITEMERGENSI_8);
-var lyr_JURITEMERGENSI_8 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_JURITEMERGENSI_8, 
-                style: style_JURITEMERGENSI_8,
-                interactive: true,
-                title: '<img src="styles/legend/JURITEMERGENSI_8.png" /> JURIT EMERGENSI'
-            });
-var format_NAVDAREMERGENSI_9 = new ol.format.GeoJSON();
-var features_NAVDAREMERGENSI_9 = format_NAVDAREMERGENSI_9.readFeatures(json_NAVDAREMERGENSI_9, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_NAVDAREMERGENSI_9 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_NAVDAREMERGENSI_9.addFeatures(features_NAVDAREMERGENSI_9);
-var lyr_NAVDAREMERGENSI_9 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_NAVDAREMERGENSI_9, 
-                style: style_NAVDAREMERGENSI_9,
-                interactive: true,
-                title: '<img src="styles/legend/NAVDAREMERGENSI_9.png" /> NAVDAR EMERGENSI'
-            });
-var format_JalanDroppointBRapelling_10 = new ol.format.GeoJSON();
-var features_JalanDroppointBRapelling_10 = format_JalanDroppointBRapelling_10.readFeatures(json_JalanDroppointBRapelling_10, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_JalanDroppointBRapelling_10 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_JalanDroppointBRapelling_10.addFeatures(features_JalanDroppointBRapelling_10);
-var lyr_JalanDroppointBRapelling_10 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_JalanDroppointBRapelling_10, 
-                style: style_JalanDroppointBRapelling_10,
-                interactive: true,
-                title: '<img src="styles/legend/JalanDroppointBRapelling_10.png" /> Jalan Droppoint-B-Rapelling'
-            });
 
-        var lyr_GoogleSatelliteHybrid_11 = new ol.layer.Tile({
+        var lyr_GoogleSatelliteHybrid_3 = new ol.layer.Tile({
             'title': 'Google Satellite Hybrid',
             'type': 'base',
             'opacity': 1.000000,
@@ -166,51 +54,157 @@ var lyr_JalanDroppointBRapelling_10 = new ol.layer.Vector({
                 url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
             })
         });
-var group_PLANEmergensi = new ol.layer.Group({
-                                layers: [lyr_JURITEMERGENSI_8,lyr_NAVDAREMERGENSI_9,lyr_JalanDroppointBRapelling_10,],
-                                title: "PLAN Emergensi"});
-var group_PLANAB = new ol.layer.Group({
-                                layers: [lyr_JalanDroppointAkeWelcoming_3,lyr_JURITPLANB_4,lyr_JURITPLANA_5,lyr_ShortcutPanitia_6,lyr_NAVDAR12_7,],
-                                title: "PLAN A, B"});
+var format_JalanDroppointAkeWelcoming_4 = new ol.format.GeoJSON();
+var features_JalanDroppointAkeWelcoming_4 = format_JalanDroppointAkeWelcoming_4.readFeatures(json_JalanDroppointAkeWelcoming_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JalanDroppointAkeWelcoming_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JalanDroppointAkeWelcoming_4.addFeatures(features_JalanDroppointAkeWelcoming_4);
+var lyr_JalanDroppointAkeWelcoming_4 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_JalanDroppointAkeWelcoming_4, 
+                style: style_JalanDroppointAkeWelcoming_4,
+                interactive: true,
+                title: '<img src="styles/legend/JalanDroppointAkeWelcoming_4.png" /> Jalan Droppoint-A ke Welcoming'
+            });
+var format_JURITPLANB_5 = new ol.format.GeoJSON();
+var features_JURITPLANB_5 = format_JURITPLANB_5.readFeatures(json_JURITPLANB_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JURITPLANB_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JURITPLANB_5.addFeatures(features_JURITPLANB_5);
+var lyr_JURITPLANB_5 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_JURITPLANB_5, 
+                style: style_JURITPLANB_5,
+                interactive: true,
+                title: '<img src="styles/legend/JURITPLANB_5.png" /> JURIT PLAN-B'
+            });
+var format_JURITPLANA_6 = new ol.format.GeoJSON();
+var features_JURITPLANA_6 = format_JURITPLANA_6.readFeatures(json_JURITPLANA_6, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JURITPLANA_6 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JURITPLANA_6.addFeatures(features_JURITPLANA_6);
+var lyr_JURITPLANA_6 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_JURITPLANA_6, 
+                style: style_JURITPLANA_6,
+                interactive: true,
+                title: '<img src="styles/legend/JURITPLANA_6.png" /> JURIT PLAN-A'
+            });
+var format_ShortcutPanitia_7 = new ol.format.GeoJSON();
+var features_ShortcutPanitia_7 = format_ShortcutPanitia_7.readFeatures(json_ShortcutPanitia_7, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ShortcutPanitia_7 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ShortcutPanitia_7.addFeatures(features_ShortcutPanitia_7);
+var lyr_ShortcutPanitia_7 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_ShortcutPanitia_7, 
+                style: style_ShortcutPanitia_7,
+                interactive: true,
+                title: '<img src="styles/legend/ShortcutPanitia_7.png" /> Shortcut Panitia'
+            });
+var format_NAVDAR12_8 = new ol.format.GeoJSON();
+var features_NAVDAR12_8 = format_NAVDAR12_8.readFeatures(json_NAVDAR12_8, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_NAVDAR12_8 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_NAVDAR12_8.addFeatures(features_NAVDAR12_8);
+var lyr_NAVDAR12_8 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_NAVDAR12_8, 
+                style: style_NAVDAR12_8,
+                interactive: true,
+                title: '<img src="styles/legend/NAVDAR12_8.png" /> NAVDAR 1-2'
+            });
+var format_JURITEMERGENSI_9 = new ol.format.GeoJSON();
+var features_JURITEMERGENSI_9 = format_JURITEMERGENSI_9.readFeatures(json_JURITEMERGENSI_9, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JURITEMERGENSI_9 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JURITEMERGENSI_9.addFeatures(features_JURITEMERGENSI_9);
+var lyr_JURITEMERGENSI_9 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_JURITEMERGENSI_9, 
+                style: style_JURITEMERGENSI_9,
+                interactive: true,
+                title: '<img src="styles/legend/JURITEMERGENSI_9.png" /> JURIT EMERGENSI'
+            });
+var format_NAVDAREMERGENSI_10 = new ol.format.GeoJSON();
+var features_NAVDAREMERGENSI_10 = format_NAVDAREMERGENSI_10.readFeatures(json_NAVDAREMERGENSI_10, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_NAVDAREMERGENSI_10 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_NAVDAREMERGENSI_10.addFeatures(features_NAVDAREMERGENSI_10);
+var lyr_NAVDAREMERGENSI_10 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_NAVDAREMERGENSI_10, 
+                style: style_NAVDAREMERGENSI_10,
+                interactive: true,
+                title: '<img src="styles/legend/NAVDAREMERGENSI_10.png" /> NAVDAR EMERGENSI'
+            });
+var format_JalanDroppointBRapelling_11 = new ol.format.GeoJSON();
+var features_JalanDroppointBRapelling_11 = format_JalanDroppointBRapelling_11.readFeatures(json_JalanDroppointBRapelling_11, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JalanDroppointBRapelling_11 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JalanDroppointBRapelling_11.addFeatures(features_JalanDroppointBRapelling_11);
+var lyr_JalanDroppointBRapelling_11 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_JalanDroppointBRapelling_11, 
+                style: style_JalanDroppointBRapelling_11,
+                interactive: true,
+                title: '<img src="styles/legend/JalanDroppointBRapelling_11.png" /> Jalan Droppoint-B-Rapelling'
+            });
 var group_OLD = new ol.layer.Group({
                                 layers: [lyr_NAVDAR12danJURIT1tracks_0,lyr_JURIT2tracks_1,lyr_JURITEMERGENSItracks_2,],
                                 title: "OLD"});
 
-lyr_NAVDAR12danJURIT1tracks_0.setVisible(true);lyr_JURIT2tracks_1.setVisible(true);lyr_JURITEMERGENSItracks_2.setVisible(true);lyr_JalanDroppointAkeWelcoming_3.setVisible(true);lyr_JURITPLANB_4.setVisible(true);lyr_JURITPLANA_5.setVisible(true);lyr_ShortcutPanitia_6.setVisible(true);lyr_NAVDAR12_7.setVisible(true);lyr_JURITEMERGENSI_8.setVisible(true);lyr_NAVDAREMERGENSI_9.setVisible(true);lyr_JalanDroppointBRapelling_10.setVisible(true);lyr_GoogleSatelliteHybrid_11.setVisible(true);
-var layersList = [group_OLD,group_PLANAB,group_PLANEmergensi,lyr_GoogleSatelliteHybrid_11];
+lyr_NAVDAR12danJURIT1tracks_0.setVisible(true);lyr_JURIT2tracks_1.setVisible(true);lyr_JURITEMERGENSItracks_2.setVisible(true);lyr_GoogleSatelliteHybrid_3.setVisible(true);lyr_JalanDroppointAkeWelcoming_4.setVisible(true);lyr_JURITPLANB_5.setVisible(true);lyr_JURITPLANA_6.setVisible(true);lyr_ShortcutPanitia_7.setVisible(true);lyr_NAVDAR12_8.setVisible(true);lyr_JURITEMERGENSI_9.setVisible(true);lyr_NAVDAREMERGENSI_10.setVisible(true);lyr_JalanDroppointBRapelling_11.setVisible(true);
+var layersList = [group_OLD,lyr_GoogleSatelliteHybrid_3,lyr_JalanDroppointAkeWelcoming_4,lyr_JURITPLANB_5,lyr_JURITPLANA_6,lyr_ShortcutPanitia_7,lyr_NAVDAR12_8,lyr_JURITEMERGENSI_9,lyr_NAVDAREMERGENSI_10,lyr_JalanDroppointBRapelling_11];
 lyr_NAVDAR12danJURIT1tracks_0.set('fieldAliases', {'name': 'name', 'cmt': 'cmt', 'desc': 'desc', 'src': 'src', 'link1_href': 'link1_href', 'link1_text': 'link1_text', 'link1_type': 'link1_type', 'link2_href': 'link2_href', 'link2_text': 'link2_text', 'link2_type': 'link2_type', 'number': 'number', 'type': 'type', });
 lyr_JURIT2tracks_1.set('fieldAliases', {'name': 'name', 'cmt': 'cmt', 'desc': 'desc', 'src': 'src', 'link1_href': 'link1_href', 'link1_text': 'link1_text', 'link1_type': 'link1_type', 'link2_href': 'link2_href', 'link2_text': 'link2_text', 'link2_type': 'link2_type', 'number': 'number', 'type': 'type', });
 lyr_JURITEMERGENSItracks_2.set('fieldAliases', {'name': 'name', 'cmt': 'cmt', 'desc': 'desc', 'src': 'src', 'link1_href': 'link1_href', 'link1_text': 'link1_text', 'link1_type': 'link1_type', 'link2_href': 'link2_href', 'link2_text': 'link2_text', 'link2_type': 'link2_type', 'number': 'number', 'type': 'type', });
-lyr_JalanDroppointAkeWelcoming_3.set('fieldAliases', {'name': 'name', });
-lyr_JURITPLANB_4.set('fieldAliases', {'nama': 'nama', });
-lyr_JURITPLANA_5.set('fieldAliases', {'nama': 'nama', });
-lyr_ShortcutPanitia_6.set('fieldAliases', {'nama_jalan': 'nama_jalan', });
-lyr_NAVDAR12_7.set('fieldAliases', {'nama': 'nama', });
-lyr_JURITEMERGENSI_8.set('fieldAliases', {'nama': 'nama', });
-lyr_NAVDAREMERGENSI_9.set('fieldAliases', {'nama': 'nama', });
-lyr_JalanDroppointBRapelling_10.set('fieldAliases', {'nama': 'nama', });
+lyr_JalanDroppointAkeWelcoming_4.set('fieldAliases', {'name': 'name', });
+lyr_JURITPLANB_5.set('fieldAliases', {'nama': 'nama', });
+lyr_JURITPLANA_6.set('fieldAliases', {'nama': 'nama', });
+lyr_ShortcutPanitia_7.set('fieldAliases', {'nama_jalan': 'nama_jalan', });
+lyr_NAVDAR12_8.set('fieldAliases', {'nama': 'nama', });
+lyr_JURITEMERGENSI_9.set('fieldAliases', {'nama': 'nama', });
+lyr_NAVDAREMERGENSI_10.set('fieldAliases', {'nama': 'nama', });
+lyr_JalanDroppointBRapelling_11.set('fieldAliases', {'nama': 'nama', });
 lyr_NAVDAR12danJURIT1tracks_0.set('fieldImages', {'name': 'TextEdit', 'cmt': 'TextEdit', 'desc': 'TextEdit', 'src': 'TextEdit', 'link1_href': 'TextEdit', 'link1_text': 'TextEdit', 'link1_type': 'TextEdit', 'link2_href': 'TextEdit', 'link2_text': 'TextEdit', 'link2_type': 'TextEdit', 'number': 'Range', 'type': 'TextEdit', });
 lyr_JURIT2tracks_1.set('fieldImages', {'name': 'TextEdit', 'cmt': 'TextEdit', 'desc': 'TextEdit', 'src': 'TextEdit', 'link1_href': 'TextEdit', 'link1_text': 'TextEdit', 'link1_type': 'TextEdit', 'link2_href': 'TextEdit', 'link2_text': 'TextEdit', 'link2_type': 'TextEdit', 'number': 'Range', 'type': 'TextEdit', });
 lyr_JURITEMERGENSItracks_2.set('fieldImages', {'name': 'TextEdit', 'cmt': 'TextEdit', 'desc': 'TextEdit', 'src': 'TextEdit', 'link1_href': 'TextEdit', 'link1_text': 'TextEdit', 'link1_type': 'TextEdit', 'link2_href': 'TextEdit', 'link2_text': 'TextEdit', 'link2_type': 'TextEdit', 'number': 'Range', 'type': 'TextEdit', });
-lyr_JalanDroppointAkeWelcoming_3.set('fieldImages', {'name': 'TextEdit', });
-lyr_JURITPLANB_4.set('fieldImages', {'nama': 'TextEdit', });
-lyr_JURITPLANA_5.set('fieldImages', {'nama': 'TextEdit', });
-lyr_ShortcutPanitia_6.set('fieldImages', {'nama_jalan': 'TextEdit', });
-lyr_NAVDAR12_7.set('fieldImages', {'nama': 'TextEdit', });
-lyr_JURITEMERGENSI_8.set('fieldImages', {'nama': 'TextEdit', });
-lyr_NAVDAREMERGENSI_9.set('fieldImages', {'nama': 'TextEdit', });
-lyr_JalanDroppointBRapelling_10.set('fieldImages', {'nama': 'TextEdit', });
+lyr_JalanDroppointAkeWelcoming_4.set('fieldImages', {'name': 'TextEdit', });
+lyr_JURITPLANB_5.set('fieldImages', {'nama': 'TextEdit', });
+lyr_JURITPLANA_6.set('fieldImages', {'nama': 'TextEdit', });
+lyr_ShortcutPanitia_7.set('fieldImages', {'nama_jalan': 'TextEdit', });
+lyr_NAVDAR12_8.set('fieldImages', {'nama': 'TextEdit', });
+lyr_JURITEMERGENSI_9.set('fieldImages', {'nama': 'TextEdit', });
+lyr_NAVDAREMERGENSI_10.set('fieldImages', {'nama': 'TextEdit', });
+lyr_JalanDroppointBRapelling_11.set('fieldImages', {'nama': 'TextEdit', });
 lyr_NAVDAR12danJURIT1tracks_0.set('fieldLabels', {});
 lyr_JURIT2tracks_1.set('fieldLabels', {});
 lyr_JURITEMERGENSItracks_2.set('fieldLabels', {});
-lyr_JalanDroppointAkeWelcoming_3.set('fieldLabels', {});
-lyr_JURITPLANB_4.set('fieldLabels', {});
-lyr_JURITPLANA_5.set('fieldLabels', {});
-lyr_ShortcutPanitia_6.set('fieldLabels', {});
-lyr_NAVDAR12_7.set('fieldLabels', {});
-lyr_JURITEMERGENSI_8.set('fieldLabels', {});
-lyr_NAVDAREMERGENSI_9.set('fieldLabels', {});
-lyr_JalanDroppointBRapelling_10.set('fieldLabels', {});
-lyr_JalanDroppointBRapelling_10.on('precompose', function(evt) {
+lyr_JalanDroppointAkeWelcoming_4.set('fieldLabels', {'name': 'no label', });
+lyr_JURITPLANB_5.set('fieldLabels', {'nama': 'no label', });
+lyr_JURITPLANA_6.set('fieldLabels', {'nama': 'no label', });
+lyr_ShortcutPanitia_7.set('fieldLabels', {'nama_jalan': 'no label', });
+lyr_NAVDAR12_8.set('fieldLabels', {'nama': 'no label', });
+lyr_JURITEMERGENSI_9.set('fieldLabels', {'nama': 'no label', });
+lyr_NAVDAREMERGENSI_10.set('fieldLabels', {'nama': 'header label', });
+lyr_JalanDroppointBRapelling_11.set('fieldLabels', {'nama': 'no label', });
+lyr_JalanDroppointBRapelling_11.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ styles/JalanDroppointBRapelling_11_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_JalanDroppointBRapelling_11 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "15.600000000000001px \'Arial\', sans-serif";
+    var labelFill = "#0091ca";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 4.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if (feature.get("nama") !== null) {
+        labelText = String(feature.get("nama"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(17,21,255,1.0)', lineDash: null, lineCap: 'round', lineJoin: 'round', width: 5}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
